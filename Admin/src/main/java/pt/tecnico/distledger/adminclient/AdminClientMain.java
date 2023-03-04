d035cc4be8147e6faf0d915175031709a694c50f
Diogo Correia <me@diogotc.com>
2023-03-04 01:22:19 +0000

    chore: tweak formatter settings (again)

--- Admin/src/main/java/pt/tecnico/distledger/adminclient/AdminClientMain.java
+++ Admin/src/main/java/pt/tecnico/distledger/adminclient/AdminClientMain.java
@@ -9,9 +9,7 @@ public class AdminClientMain {
 
         // receive and print arguments
         System.out.printf("Received %d arguments%n", args.length);
-        for (
-                int i = 0; i < args.length; i++
-        ) {
+        for (int i = 0; i < args.length; i++) {
             System.out.printf("arg[%d] = %s%n", i, args[i]);
         }
 
