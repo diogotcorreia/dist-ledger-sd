aed33e0844026e696529ab6c3d6a462dccfe9df7
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-03-06 09:44:33 +0000

    feat(user): implement commands and service (#31)

--- User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
+++ User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
@@ -1,8 +1,69 @@
 package pt.tecnico.distledger.userclient.grpc;
 
-public class UserService {
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.BalanceRequest;
+import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.BalanceResponse;
+import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.CreateAccountRequest;
+import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.DeleteAccountRequest;
+import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.TransferToRequest;
+import pt.ulisboa.tecnico.distledger.contract.user.UserServiceGrpc;
 
-    /*TODO: The gRPC client-side logic should be here.
-        This should include a method that builds a channel and stub,
-        as well as individual methods for each remote operation of this service. */
+public class UserService extends UserServiceGrpc.UserServiceImplBase implements AutoCloseable {
+
+    private final ManagedChannel channel;
+    // blocking stub (for now)
+    private final UserServiceGrpc.UserServiceBlockingStub stub;
+
+    public UserService(String host, int port) {
+        channel = ManagedChannelBuilder.forTarget(host + ":" + port)
+                .usePlaintext()
+                .build();
+        stub = UserServiceGrpc.newBlockingStub(channel);
+    }
+
+    public void createAccount(String username) throws StatusRuntimeException {
+        //noinspection ResultOfMethodCallIgnored
+        stub.createAccount(
+                CreateAccountRequest.newBuilder()
+                        .setUserId(username)
+                        .build()
+        );
+    }
+
+    public void deleteAccount(String username) throws StatusRuntimeException {
+        //noinspection ResultOfMethodCallIgnored
+        stub.deleteAccount(
+                DeleteAccountRequest.newBuilder()
+                        .setUserId(username)
+                        .build()
+        );
+    }
+
+    public int balance(String username) throws StatusRuntimeException {
+        final BalanceResponse response = stub.balance(
+                BalanceRequest.newBuilder()
+                        .setUserId(username)
+                        .build()
+        );
+        return response.getValue();
+    }
+
+
+    public void transferTo(String username, String to, Integer amount) throws StatusRuntimeException {
+        //noinspection ResultOfMethodCallIgnored
+        stub.transferTo(
+                TransferToRequest.newBuilder()
+                        .setAccountFrom(username)
+                        .setAccountTo(to)
+                        .setAmount(amount)
+                        .build()
+        );
+    }
+
+    @Override
+    public void close() {
+        channel.shutdown();
+    }
 }
