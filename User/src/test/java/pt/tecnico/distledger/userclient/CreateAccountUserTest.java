6b44767522bd2b80aeedff514d0efbff14b7fa49
Pesteves2002 <tomasesteves2002@gmail.com>
2023-03-07 17:43:12 +0000

    Add some tests

--- User/src/test/java/pt/tecnico/distledger/userclient/CreateAccountUserTest.java
+++ User/src/test/java/pt/tecnico/distledger/userclient/CreateAccountUserTest.java
@@ -17,7 +17,7 @@ import static org.grpcmock.GrpcMock.unaryMethod;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-class UserTest {
+class CreateAccountUserTest {
 
     private static UserService service;
     private static CommandParser client;
@@ -26,6 +26,10 @@ class UserTest {
     private static ByteArrayOutputStream outputStream;
 
     private static final String LOCALHOST = "localhost";
+
+    private static final String createAccountCommand = "createAccount A user1\n";
+
+    private static final String exitCommand = "exit\n";
 
     @BeforeEach
     void setup() {
@@ -37,47 +41,23 @@ class UserTest {
         System.setOut(new PrintStream(outputStream));
     }
 
-    @Test
-    void help() {
-        final String helpCommand = """
-                help
-                exit
-                """;
-        inputStream = new ByteArrayInputStream(helpCommand.getBytes());
-        System.setIn(inputStream);
-
-        client.parseInput();
-
-        assertEquals(outputStream.toString(), """
-                > Usage:
-                - createAccount <server> <username>
-                - deleteAccount <server> <username>
-                - balance <server> <username>
-                - transferTo <server> <username_from> <username_to> <amount>
-                - exit
-
-                >\s""");
-    }
 
     @Test
-    void balance() {
+    void createAccount() {
         stubFor(
-                unaryMethod(UserServiceGrpc.getBalanceMethod())
-                        .willReturn(response(UserDistLedger.BalanceResponse.newBuilder().setValue(20).build()))
+                unaryMethod(UserServiceGrpc.getCreateAccountMethod())
+                        .withRequest(UserDistLedger.CreateAccountRequest.newBuilder().setUserId("user1").build())
+                        .willReturn(response(UserDistLedger.CreateAccountResponse.newBuilder().build()))
         );
 
-        final String balanceCommand = "balance A user1\nexit\n";
-        inputStream = new ByteArrayInputStream(balanceCommand.getBytes());
+        final String command = createAccountCommand + exitCommand;
+        inputStream = new ByteArrayInputStream(command.getBytes());
         System.setIn(inputStream);
 
         client.parseInput();
 
         assertEquals(outputStream.toString(), """
-                > OK
-                20
-
+                > Account 'user1' has been created
                 >\s""");
     }
-
-
 }
---
+++ User/src/test/java/pt/tecnico/distledger/userclient/HelpAndBalanceUserTest.java
@@ -0,0 +1,99 @@
+package pt.tecnico.distledger.userclient;
+
+import org.grpcmock.GrpcMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.tecnico.distledger.userclient.grpc.UserService;
+import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger;
+import pt.ulisboa.tecnico.distledger.contract.user.UserServiceGrpc;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.grpcmock.GrpcMock.response;
+import static org.grpcmock.GrpcMock.stubFor;
+import static org.grpcmock.GrpcMock.unaryMethod;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class HelpAndBalanceUserTest {
+
+    private static UserService service;
+    private static CommandParser client;
+
+    private static ByteArrayInputStream inputStream;
+    private static ByteArrayOutputStream outputStream;
+
+    private static final String LOCALHOST = "localhost";
+
+    @BeforeEach
+    void setup() {
+        GrpcMock.configureFor(GrpcMock.grpcMock(0).build().start());
+
+        service = new UserService(LOCALHOST, GrpcMock.getGlobalPort());
+        client = new CommandParser(service);
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @Test
+    void help() {
+        final String helpCommand = """
+                help
+                exit
+                """;
+        inputStream = new ByteArrayInputStream(helpCommand.getBytes());
+        System.setIn(inputStream);
+
+        client.parseInput();
+
+        assertEquals(outputStream.toString(), """
+                > Usage:
+                - createAccount <server> <username>
+                - deleteAccount <server> <username>
+                - balance <server> <username>
+                - transferTo <server> <username_from> <username_to> <amount>
+                - exit
+
+                >\s""");
+    }
+
+    @Test
+    void balance() {
+        stubFor(
+                unaryMethod(UserServiceGrpc.getBalanceMethod())
+                        .willReturn(response(UserDistLedger.BalanceResponse.newBuilder().setValue(20).build()))
+        );
+
+        final String balanceCommand = "balance A user1\nexit\n";
+        inputStream = new ByteArrayInputStream(balanceCommand.getBytes());
+        System.setIn(inputStream);
+
+        client.parseInput();
+
+        assertEquals(outputStream.toString(), """
+                > Balance of user 'user1' is 20
+                >\s""");
+    }
+
+    @Test
+    void emptyBalance() {
+        stubFor(
+                unaryMethod(UserServiceGrpc.getBalanceMethod())
+                        .willReturn(response(UserDistLedger.BalanceResponse.newBuilder().setValue(0).build()))
+        );
+
+        final String balanceCommand = "balance A user1\nexit\n";
+        inputStream = new ByteArrayInputStream(balanceCommand.getBytes());
+        System.setIn(inputStream);
+
+        client.parseInput();
+
+        assertEquals(outputStream.toString(), """
+                > Balance of user 'user1' is 0
+                >\s""");
+    }
+
+
+}
