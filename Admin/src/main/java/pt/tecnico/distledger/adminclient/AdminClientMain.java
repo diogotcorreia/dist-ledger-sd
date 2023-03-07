b997a5a38a00787ee2e4be5fd6041213ad005af2
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-03-07 16:03:11 +0000

    feat(admin): add debug logging (#51)

--- Admin/src/main/java/pt/tecnico/distledger/adminclient/AdminClientMain.java
+++ Admin/src/main/java/pt/tecnico/distledger/adminclient/AdminClientMain.java
@@ -1,23 +1,25 @@
 package pt.tecnico.distledger.adminclient;
 
+import lombok.CustomLog;
 import pt.tecnico.distledger.adminclient.grpc.AdminService;
+import pt.tecnico.distledger.common.Logger;
 
+@CustomLog(topic = "AdminClientMain")
 public class AdminClientMain {
     public static void main(String[] args) {
-
-        System.out.println(AdminClientMain.class.getSimpleName());
+        Logger.setDebug(System.getProperty("debug") != null);
 
         // receive and print arguments
-        System.out.printf("Received %d arguments%n", args.length);
+        log.debug("Received %d arguments", args.length);
         for (int i = 0; i < args.length; i++) {
-            System.out.printf("arg[%d] = %s%n", i, args[i]);
+            log.debug("arg[%d] = %s", i, args[i]);
         }
 
         // check arguments
         if (args.length != 2) {
-            System.err.println("Argument(s) missing!");
-            System.err.println("Usage: mvn exec:java -Dexec.args=<host> <port>");
-            return;
+            log.error("Argument(s) missing!");
+            log.error("Usage: mvn exec:java -Dexec.args=<host> <port>");
+            System.exit(1);
         }
 
         final String host = args[0];
