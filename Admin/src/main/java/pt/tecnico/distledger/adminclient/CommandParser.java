e6664a41683dbdc536f8250009f5a3f94152206a
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-03-08 10:58:00 +0000

    feat(user/admin): add command aliases (#56)

--- Admin/src/main/java/pt/tecnico/distledger/adminclient/CommandParser.java
+++ Admin/src/main/java/pt/tecnico/distledger/adminclient/CommandParser.java
@@ -11,11 +11,17 @@ public class CommandParser {
 
     private static final String SPACE = " ";
     private static final String ACTIVATE = "activate";
+    private static final String ACTIVATE_ALIAS = "a";
     private static final String DEACTIVATE = "deactivate";
+    private static final String DEACTIVATE_ALIAS = "d";
     private static final String GET_LEDGER_STATE = "getLedgerState";
+    private static final String GET_LEDGER_STATE_ALIAS = "ls";
     private static final String GOSSIP = "gossip";
+    private static final String GOSSIP_ALIAS = "g";
     private static final String HELP = "help";
+    private static final String HELP_ALIAS = "h";
     private static final String EXIT = "exit";
+    private static final String EXIT_ALIAS = "e";
 
     private final AdminService adminService;
 
@@ -33,12 +39,12 @@ public class CommandParser {
             String cmd = line.split(SPACE)[0];
 
             switch (cmd) {
-                case ACTIVATE -> this.activate(line);
-                case DEACTIVATE -> this.deactivate(line);
-                case GET_LEDGER_STATE -> this.dump(line);
-                case GOSSIP -> this.gossip(line);
-                case HELP -> this.printUsage();
-                case EXIT -> exit = true;
+                case ACTIVATE, ACTIVATE_ALIAS -> this.activate(line);
+                case DEACTIVATE, DEACTIVATE_ALIAS -> this.deactivate(line);
+                case GET_LEDGER_STATE, GET_LEDGER_STATE_ALIAS -> this.dump(line);
+                case GOSSIP, GOSSIP_ALIAS -> this.gossip(line);
+                case HELP, HELP_ALIAS -> this.printUsage();
+                case EXIT, EXIT_ALIAS -> exit = true;
                 default -> {
                     log.error("Command '%s' does not exist%n", cmd);
                     this.printUsage();
