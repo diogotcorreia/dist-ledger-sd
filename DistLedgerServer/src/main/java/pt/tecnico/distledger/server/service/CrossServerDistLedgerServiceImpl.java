6c33dceabc8c6abee477922705436114eb522e1f
Pesteves2002 <tomasesteves2002@tecnico.ulisboa.pt>
2023-03-21 21:07:19 +0000

    Add CrossServerDistLedgerServiceImpl

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/service/CrossServerDistLedgerServiceImpl.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/service/CrossServerDistLedgerServiceImpl.java
@@ -1,9 +1,19 @@
 package pt.tecnico.distledger.server.service;
 
+import io.grpc.stub.StreamObserver;
+import lombok.CustomLog;
 import pt.tecnico.distledger.server.domain.ServerState;
-import pt.ulisboa.tecnico.distledger.contract.distledgerserver.DistLedgerCrossServerServiceGrpc;
+import pt.tecnico.distledger.server.domain.operation.CreateOp;
+import pt.tecnico.distledger.server.domain.operation.DeleteOp;
+import pt.tecnico.distledger.server.domain.operation.Operation;
+import pt.tecnico.distledger.server.domain.operation.TransferOp;
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions;
+import pt.ulisboa.tecnico.distledger.contract.distledgerserver.CrossServerDistLedger.PropagateStateRequest;
+import pt.ulisboa.tecnico.distledger.contract.distledgerserver.CrossServerDistLedger.PropagateStateResponse;
+import pt.ulisboa.tecnico.distledger.contract.distledgerserver.DistLedgerCrossServerServiceGrpc.DistLedgerCrossServerServiceImplBase;
 
-public class CrossServerDistLedgerServiceImpl extends DistLedgerCrossServerServiceGrpc.DistLedgerCrossServerServiceImplBase {
+@CustomLog(topic = "Cross Server Service")
+public class CrossServerDistLedgerServiceImpl extends DistLedgerCrossServerServiceImplBase {
 
     private final ServerState serverState;
 
@@ -11,4 +21,36 @@ public class CrossServerDistLedgerServiceImpl extends DistLedgerCrossServerServi
         this.serverState = serverState;
     }
 
+    @Override
+    public void propagateState(
+            PropagateStateRequest request,
+            StreamObserver<PropagateStateResponse> responseObserver
+    ) {
+        log.debug("Propagate state has been received");
+
+        serverState.setLedger(
+                request.getState()
+                        .getLedgerList()
+                        .stream()
+                        .map(this::toOperation)
+                        .toList()
+        );
+
+        log.debug("Propagate state response has been sent");
+        responseObserver.onNext(PropagateStateResponse.getDefaultInstance());
+        responseObserver.onCompleted();
+    }
+
+    private Operation toOperation(DistLedgerCommonDefinitions.Operation operation) {
+        return switch (operation.getType()) {
+            case OP_CREATE_ACCOUNT -> new CreateOp(operation.getUserId());
+            case OP_TRANSFER_TO -> new TransferOp(
+                    operation.getDestUserId(),
+                    operation.getDestUserId(),
+                    operation.getAmount()
+            );
+            case OP_DELETE_ACCOUNT -> new DeleteOp(operation.getUserId());
+            default -> throw new IllegalArgumentException("Invalid operation");
+        };
+    }
 }
