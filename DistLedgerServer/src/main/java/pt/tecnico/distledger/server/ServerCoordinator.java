95f2b274a97756e79caf1130ff27d6b1d1201b98
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-03-22 12:38:23 +0000

    feat(server): WIP: cache server peers

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerCoordinator.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerCoordinator.java
@@ -1,14 +1,16 @@
 package pt.tecnico.distledger.server;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import lombok.Getter;
 import pt.tecnico.distledger.server.domain.ServerState;
 import pt.tecnico.distledger.server.domain.operation.Operation;
 import pt.tecnico.distledger.server.grpc.CrossServerService;
 import pt.tecnico.distledger.server.grpc.NamingServerService;
 import pt.tecnico.distledger.server.visitor.ConvertOperationsToGrpcVisitor;
-import pt.ulisboa.tecnico.distledger.contract.namingserver.NamingServerDistLedger;
+import pt.ulisboa.tecnico.distledger.contract.namingserver.NamingServerDistLedger.ServerInfo;
 
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ServerCoordinator {
 
@@ -19,6 +21,11 @@ public class ServerCoordinator {
 
     @Getter
     private final ServerState serverState;
+
+
+    private final Cache<ServerInfo, CrossServerService> peersCache = CacheBuilder.newBuilder()
+            .expireAfterWrite(2, TimeUnit.MINUTES)
+            .build();
 
     private final NamingServerService namingServerService = new NamingServerService();
 
@@ -41,23 +48,39 @@ public class ServerCoordinator {
     }
 
     public void propagateLedgerStateToAllServers(Operation pendingOperation) {
-        // TODO this uses gRPC objects. convert to domain?
-
-        List<NamingServerDistLedger.ServerInfo> serverList;
-        serverList = namingServerService.getServerList()
-                .stream()
-                .filter(serverInfo -> !serverInfo.getQualifier().equals("A"))
-                .toList();
+        if (peersCache.size() == 0) {
+            namingServerService.getServerList()
+                    .stream()
+                    .filter(serverInfo -> !serverInfo.getQualifier().equals(qualifier))
+                    .forEach(serverInfo -> peersCache.put(serverInfo, new CrossServerService(serverInfo)));
+        }
 
         ConvertOperationsToGrpcVisitor visitor = new ConvertOperationsToGrpcVisitor();
         serverState.operateOverLedger(visitor);
         pendingOperation.accept(visitor);
 
-        // TODO cache them?
-        for (NamingServerDistLedger.ServerInfo serverInfo : serverList) {
-            try (var serverService = new CrossServerService(serverInfo)) {
-                serverService.sendLedger(visitor.getLedger());
-            }
+        // TODO handle errors?
+        // TODO retry again
+        long successfulCount = peersCache.asMap()
+                .entrySet()
+                .stream()
+                .map(service -> {
+                    try {
+                        service.getValue().sendLedger(visitor.getLedger());
+                        return true;
+                    } catch (Exception e) {
+                        // TODO log?
+                        e.printStackTrace();
+                        peersCache.invalidate(service.getKey());
+                        return false;
+                    }
+                })
+                .filter(Boolean::booleanValue)
+                .count();
+
+        if (successfulCount < 1) {
+            // TODO change exception
+            throw new RuntimeException("can't propagate stuff");
         }
     }
 
