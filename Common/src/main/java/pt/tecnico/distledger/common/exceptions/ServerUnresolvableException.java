d8bd1bea08c7fc8f7788a818de5f0ee9bb267d30
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-03-23 15:23:10 +0000

    fix(user): fix typo
    Co-authored-by: Tomás Esteves <tomas.esteves2002@tecnico.ulisboa.pt>
    
    Co-authored-by: Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>

--- Common/src/main/java/pt/tecnico/distledger/common/exceptions/ServerUnresolvableException.java
+++ Common/src/main/java/pt/tecnico/distledger/common/exceptions/ServerUnresolvableException.java
@@ -3,6 +3,6 @@ package pt.tecnico.distledger.common.exceptions;
 public class ServerUnresolvableException extends Exception {
 
     public ServerUnresolvableException(String qualifier) {
-        super(String.format("Cannot resolver server with qualifier '%s'", qualifier));
+        super(String.format("Cannot resolve server with qualifier '%s'", qualifier));
     }
 }
