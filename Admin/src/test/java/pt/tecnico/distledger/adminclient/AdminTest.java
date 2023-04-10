a4148bb1e9de234652d880fbd38902065ad6ccd4
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-04-10 11:03:11 +0100

    fix(admin): change toString in admin test

--- Admin/src/test/java/pt/tecnico/distledger/adminclient/AdminTest.java
+++ Admin/src/test/java/pt/tecnico/distledger/adminclient/AdminTest.java
@@ -62,7 +62,7 @@ class AdminTest {
                 - activate <server>
                 - deactivate <server>
                 - getLedgerState <server>
-                - gossip <server>
+                - gossip <serverFrom> <serverTo>
                 - exit
 
                 >\s""", outputStream.toString());
