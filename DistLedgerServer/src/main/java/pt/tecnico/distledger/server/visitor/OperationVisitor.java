52cf89525c2380b5be721273598c846e85a90659
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2023-04-13 18:06:03 +0100

    refactor: remove delete operation (#119)

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/visitor/OperationVisitor.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/visitor/OperationVisitor.java
@@ -1,14 +1,11 @@
 package pt.tecnico.distledger.server.visitor;
 
 import pt.tecnico.distledger.server.domain.operation.CreateOp;
-import pt.tecnico.distledger.server.domain.operation.DeleteOp;
 import pt.tecnico.distledger.server.domain.operation.TransferOp;
 
 public abstract class OperationVisitor {
 
     public abstract void visit(CreateOp operation);
-
-    public abstract void visit(DeleteOp operation);
 
     public abstract void visit(TransferOp operation);
 
