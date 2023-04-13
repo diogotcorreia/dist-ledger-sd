748d7f32622308dd0156fd6acda500deb5e73a32
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-04-13 12:55:11 +0100

    Remove observer logic

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/observer/Observer.java
+++
@@ -1,8 +0,0 @@
-package pt.tecnico.distledger.server.observer;
-
-import pt.tecnico.distledger.common.VectorClock;
-import pt.tecnico.distledger.server.visitor.OperationVisitor;
-
-public interface Observer {
-    boolean update(OperationVisitor visitor, VectorClock valueTimestamp);
-}
--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/observer/ObserverManager.java
+++
@@ -1,9 +0,0 @@
-package pt.tecnico.distledger.server.observer;
-
-public interface ObserverManager {
-    void registerObserver(Observer observer);
-
-    void removeObserver(Observer observer);
-
-    void notifyObservers();
-}
