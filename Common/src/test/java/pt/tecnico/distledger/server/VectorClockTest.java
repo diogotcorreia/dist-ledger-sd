15e0cd54050d74a5c7384a3d28ea8be0f31e1319
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-04-13 12:30:06 +0100

    test(common): only add non-zero timestamps to vector clock

--- Common/src/test/java/pt/tecnico/distledger/server/VectorClockTest.java
+++ Common/src/test/java/pt/tecnico/distledger/server/VectorClockTest.java
@@ -168,4 +168,31 @@ public class VectorClockTest {
 
         assertFalse(vectorClock1.isNewerThanOrEqualTo(vectorClock2));
     }
+
+    public void areVectorClocksEqualWithAndWithoutZeroTimestamp() {
+        val vectorClock1 = new VectorClock();
+        vectorClock1.setValue("A", 9);
+        vectorClock1.setValue("B", 6);
+        vectorClock1.setValue("C", 4);
+
+        val vectorClock2 = new VectorClock();
+        vectorClock2.setValue("A", 9);
+        vectorClock2.setValue("B", 6);
+        vectorClock2.setValue("C", 4);
+        vectorClock2.setValue("D", 0);
+
+        assertEquals(vectorClock1, vectorClock2);
+    }
+
+    public void updateVectorClockWithZeroTimestamp() {
+        val vectorClock1 = new VectorClock();
+        vectorClock1.setValue("A", 9);
+        vectorClock1.setValue("B", 6);
+        vectorClock1.setValue("C", 1);
+        vectorClock1.setValue("C", 0);
+
+        assertEquals(9, vectorClock1.getValue("A"));
+        assertEquals(6, vectorClock1.getValue("B"));
+        assertEquals(0, vectorClock1.getValue("C"));
+    }
 }
